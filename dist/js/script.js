16875c7e35ebc0ba6b36015fef125fa236a022bd
salmunbarnabas <student.barnabas@gmail.com>
2022-04-20 11:40:27 +0700

    nav bar and bottom back to top

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function() {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
     if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
     }
 };
 
@@ -19,3 +24,10 @@ hamburger.addEventListener('click', function() {
   navMenu.classList.toggle('hidden');  
 });
 
+//Klik di luar hamburger
+window.addEventListener('click', function (e) {
+    if (e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');     
+    }
+});
